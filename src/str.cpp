667db181573571b7a1619b71a7af14834c4e7498
MFori <forejt.martin97@gmail.com>
2020-12-09 23:37:49 +0100

    move str join to str.cpp/h

--- src/str.cpp
+++ src/str.cpp
@@ -51,4 +51,36 @@ namespace mlang {
         return const_ptr_8;
     }
 
+    llvm::Value *StringJoin::codeGen(CodeGenContext &context) {
+
+        std::vector<llvm::Value *> strings;
+        llvm::Value *length = llvm::ConstantInt::get(llvm::Type::getInt64Ty(context.getGlobalContext()), 0);
+        llvm::Type *strType = llvm::Type::getInt8PtrTy(context.getGlobalContext());
+
+        for (auto expr : *args) {
+            auto arg = expr->codeGen(context);
+            if (arg == nullptr) {
+                return nullptr;
+            }
+
+            if (arg->getType() != strType) {
+                Node::printError(location, " invalid join string variable type ");
+                context.addError();
+                return nullptr;
+            }
+
+            strings.push_back(arg);
+
+            auto fun = (context.getModule()->getOrInsertFunction("len",
+                                                                 llvm::Type::getInt64Ty(context.getGlobalContext()),strType));
+            std::vector<llvm::Value *> fargs;
+            fargs.push_back(arg);
+            llvm::Value *len = llvm::CallInst::Create(fun, fargs, "len", context.currentBlock());
+
+            length = llvm::BinaryOperator::Create(llvm::Instruction::Add, length, len, "mathtmp", context.currentBlock());
+        }
+
+        return length;
+    }
+
 }
--- src/str.h
+++ src/str.h
@@ -7,13 +7,15 @@
 #ifndef MLANG_STR_H
 #define MLANG_STR_H
 
+#include <utility>
+
 #include "ast.h"
 
 namespace mlang {
 
     class String : public Expression {
     public:
-        explicit String(const std::string &value) : value(value) {}
+        explicit String(std::string value) : value(std::move(value)) {}
 
         ~String() override = default;
 
@@ -27,6 +29,31 @@ namespace mlang {
 
     private:
         std::string value;
+    };
+
+    class StringJoin : public Expression {
+    public:
+        StringJoin(ExpressionList *args, YYLTYPE location) : args(args), location(std::move(location)) {}
+
+        ~StringJoin() override {
+            for (auto i : *args) {
+                delete i;
+            }
+            args->clear();
+            delete args;
+        }
+
+        llvm::Value *codeGen(CodeGenContext &context) override;
+
+        NodeType getType() override { return NodeType::EXPRESSION; }
+
+        std::string toString() override { return "string join"; }
+
+        void accept(Visitor &v) override { v.visitStringJoin(this); }
+
+    private:
+        ExpressionList *args{nullptr};
+        YYLTYPE location;
     };
 
 }
